8b46906c808d80a1afb7191d80104d56499fc8a8
xiaochou <xiaochou@qq.com>
2022-03-11 16:43:09 +0800

    first commit

---
+++ src/main/java/com/xiaochou/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.xiaochou.git;
+
+/**
+ * @author xiaochou
+ * @create 2022-03-11-16:39
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+    }
+}
